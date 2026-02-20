e1b5e76a0cbf52bb48179ff7b85074554b936f7d
ionut <rautuionut7@gmail.com>
2026-02-20 05:08:21 +0200

    nigga

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
 const container = document.querySelector(".container")
 
-for (let i = 1; i < 256; i++) {
+const makeGrid = () => {
+  const input = parseInt(prompt("Choose the number of squares per each side"))
+
+  for ( let i = 0; i < input * input; i++) {
   const square = document.createElement("div")
-  square.setAttribute("id", "square" + i)
   square.classList.add("square")
   container.append(square)
+  }
+
+  let squares = document.querySelectorAll(".square")
+  Array.from(squares).map((sq) => {
+    sq.style.border = "0.5px solid black "
+    sq.style.width = "30px"
+    sq.style.height = "30px"
+  })
+
+  container.style.display = "flex"
+  container.style.flexDirection = "row"
+  container.style.flexWrap = "wrap"
+  container.style.width = `${input * 31.7}px`
+
+  Array.from(squares).map(sq => {
+  sq.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = "black"
+    console.log("event happened")
+  })
+})
 }
 
-let squares = document.querySelectorAll(".square")
-Array.from(squares).map((sq) => {
-  sq.style.border = "0.5px solid black "
-  sq.style.width = "30px"
-  sq.style.height = "30px"
-})
+const btn = document.querySelector("#btn")
 
-container.style.display = "flex"
-container.style.flexDirection = "row"
-container.style.flexWrap = "wrap"
-container.style.width = "480px"
-
+btn.addEventListener("click", () => makeGrid())
