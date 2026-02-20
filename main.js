750a3f1eb3f7339693cb116038b0c0b069d1e6ab
ionut <rautuionut7@gmail.com>
2026-02-20 05:56:13 +0200

    finished the project

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
 const container = document.querySelector(".container")
 
 const makeGrid = () => {
-  let squaresFlush = document.querySelectorAll(".square")
+  const squaresFlush = document.querySelectorAll(".square")
   Array.from(squaresFlush).map(sq => sq.remove())
   const input = parseInt(prompt("Choose the number of squares per each side"))
 
-  for ( let i = 0; i < input * input; i++) {
+  for ( const i = 0; i < input * input; i++) {
   const square = document.createElement("div")
   square.classList.add("square")
   container.append(square)
   }
 
-  let squares = document.querySelectorAll(".square")
+   const containerSize = 480
+   const squareSize = containerSize / input
+
+  const squares = document.querySelectorAll(".square")
   Array.from(squares).map((sq) => {
-    sq.style.border = "0.5px solid black "
-    sq.style.width = "30px"
-    sq.style.height = "30px"
+    sq.style.border = "0.5px solid black"
+    sq.style.width = squareSize + "px"
+    sq.style.height = squareSize + "px"
   })
 
   container.style.display = "flex"
   container.style.flexDirection = "row"
   container.style.flexWrap = "wrap"
-  container.style.width = `${input * 34}px`
+  container.style.width = containerSize + "px"
 
   Array.from(squares).map(sq => {
   sq.addEventListener("mouseover", (e) => {
